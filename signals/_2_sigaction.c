4dc4bd9876a4b3e4b6cc02cb90d60df3341df1a6
ahmed-on-github <159645855+ahmed-on-github@users.noreply.github.com>
2026-07-12 23:56:07 +0300

    Modify signal handling and blocking logic
    Updated signal handler to ignore SIGFPE and added logic for blocking/unblocking SIGHUP signals.

--- signals/_2_sigaction.c
+++ signals/_2_sigaction.c
@@ -6,33 +6,62 @@
 #include <signal.h>
 #include <errno.h>
 
-void myhandler(int signum){
+#define RESTART_LOGIC  0
 
-        //printf("Signal  caught\n");
-        printf("Signal %s caught\n", strsignal(signum));
+void myhandler(int signum) {
+
+        printf("Signal %s caught, ignoring SIGFPE\n", strsignal(signum));
+        sleep(10);/* To test that SIGFPE is ignored here */
+
+        printf("signal handler returning ...\n");
 }
 
-int main(){
+int main() {
         struct sigaction action;
 
         action.sa_handler = myhandler;
         sigemptyset(&action.sa_mask);
-        action.sa_flags = SA_RESTART; // Or 0;
+        sigaddset(&action.sa_mask, SIGFPE);     /* block  */
+        action.sa_flags = SA_RESTART;   // Or 0;
 
         int fd[2], n;
-        char buf [20];
+        char buf[20];
         pipe(fd);
 
-        sigaction(SIGINT, &action, NULL /* old sigaction ignored*/);
-        sigaction(SIGHUP, &action, NULL /* old sigaction ignored*/);
+        sigaction(SIGINT, &action, NULL /* old sigaction ignored */ );
+        sigaction(SIGHUP, &action, NULL /* old sigaction ignored */ );
 
-
-      while(1){
-              /* blocking system call as reading from the read end of pipe will block (not connected to any file/stream) */
-              n = read(fd[0], buf, sizeof(buf )); 
-              /* Won't be reached if action.sa_flags has SA_RESTART bit set*/
-              perror("read: ");
-              printf("read %d bytes, errno = %d\n", n , errno); 
+        int i = 0;
+        sigset_t sigmask;
+        /* block and unblock sighup */
+        sigemptyset(&sigmask);
+        sigaddset(&sigmask, SIGHUP);
+        while (1) {
+                /* Testing signal blocking */
+                if (RESTART_LOGIC == 0) {
+                        i++;
+                        switch (i % 5) {
+                        case 2:
+                                /* block sighup */
+                                sigprocmask(SIG_BLOCK, &sigmask, NULL /*  */ );
+                                break;
+                        case 4:
+                                /* unblock sighup */
+                                sigprocmask(SIG_UNBLOCK, &sigmask, NULL /*  */ );
+                                break;
+                        default:                        /* nothing */
+                                break;
+                        }
+                        printf("loop iteration%5: %d\n", i % 5);
+                        sleep(3);
+                }
+                else {  /* Testing system calls restart upon signal interrution */
+                        /* blocking system call as reading from the read end of pipe will block (not connected to any file/stream) */
+                        n = read(fd[0], buf, sizeof(buf));
+                        /* Won't be reached if action.sa_flags has SA_RESTART bit set */
+                        perror("read: ");
+                        printf("read %d bytes, errno = %d\n", n, errno);
+                }
         }
 
         return 0;
