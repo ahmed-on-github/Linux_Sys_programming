b2e7efa2efa72d3b9526dac6dc6c929ee43b7b8b
ahmed-on-github <159645855+ahmed-on-github@users.noreply.github.com>
2026-07-29 09:31:45 +0300

    Refactor input handling and signal management
    Refactor input handling and improve signal management in main_client.c. Added support for redirected input and improved buffer management.

--- pipes_fifos/fifo1/main_client.c
+++ pipes_fifos/fifo1/main_client.c
@@ -21,6 +21,8 @@
 #define SERVER_FIFO "/tmp/addition_server_fifo"
 #define MAX_NUMBERS 512U
 
+//#define INPUT_REDIRECT_FLAG
+
 static char fifo_buf [4096];
 
 int fd_client, fd_server, bytes ;
@@ -52,17 +54,18 @@ void signal_handler(int signum){
     close(fd_client); close(fd_server);
 }
 int main(int argc, char **argv){
+    char *fgets_ptr = NULL;
+
 
     struct sigaction signal_struct;
     signal_struct.sa_flags = 0;
     signal_struct.sa_handler = signal_handler;
 
-    sigaction(SIGINT, &signal_struct, NULL);
     sigaction(SIGTERM, &signal_struct, NULL);
 
     /* Create Fifo for client process */
     snprintf(client_fifo_name, sizeof(client_fifo_name)
-    , "/tmp/addition_client_fifo_%ld", getpid());
+    , "/tmp/addition_client_fifo_%d", getpid());
 
     /* Create fifo */
     if ( mkfifo(client_fifo_name, 0644) < 0
@@ -91,42 +94,88 @@ int main(int argc, char **argv){
 
     memcpy(fifo_buf, client_fifo_name, client_fifo_name_len);
     fifo_buf[client_fifo_name_len] = ' '; /*separate fifo name with a space*/
+
+    /* Simulate  redirected input */
+
+    FILE *input_fptr = stdin;
+    #ifdef INPUT_REDIRECT_FLAG
+    int input_fd =  open("./testfile", O_RDONLY);
+    int stdin_pipe [2];
+    pipe(stdin_pipe);
+    dup2(stdin_pipe[0], STDIN_FILENO);
+    close(stdin_pipe[1]); /* close writing end */
+
+    input_fptr = fdopen(input_fd, "r");
+    #endif /* INPUT_REDIRECT_FLAG */
+
     /* read buffer */
     if(argc == 1){
-        while( printf("Enter numbers: ") && fgets(fifo_buf + client_fifo_name_len +1,
-        4096 -  client_fifo_name_len - 1, stdin) != NULL ){
         /*
         while( printf("Enter numbers: ") && fread(fifo_buf + client_fifo_name_len + 1,
                             1, 4096 -  client_fifo_name_len - 1,
-                            stdin) > 0 ){ */ /* fread does not stop on \n as it reads binary streams*/
-
-            fifo_buf[ strlen(fifo_buf) - 1 ] = '\0'; /*terminate \n in order to allow next strlen()  call to return 0 on just pressing enter*/
-            if( strlen(fifo_buf + client_fifo_name_len +1) == 0 ) { //printf("\n");
-                continue;
+                            stdin) > 0 ){ */ /* fread() does not stop on \n */
+        int started_flag = 0;
+        int feof_flag = 0;
+        do{
+            if(started_flag == 0){
+                printf("Enter numbers: "); fflush(stdout);
+                fgets_ptr = fgets(fifo_buf + client_fifo_name_len +1,
+                                  4096 -  client_fifo_name_len - 1, input_fptr);
             }
+            fifo_buf[ strlen(fifo_buf) - 1 ] = '\0'; /*terminate \n in order to allow next strlen()  call to return 0 on just pressing enter (\n)*/
+            if( strlen(fifo_buf + client_fifo_name_len +1) == 0 ) {
+                goto retry; /* continue; //  for older vesrion with while() */ 
+            }
+            /* save redirected input from a file before getting out of the do-while loop
+               in order to print the output before the sum */
+            memcpy(tmp_fifo_buf,fifo_buf + client_fifo_name_len +1, strlen(fifo_buf + client_fifo_name_len +1));
+
             operation();
 
-            if(bytes == 0) { printf("\n"); continue;}
+            if(bytes == 0) { printf("\n"); goto retry; }
 
             fifo_buf[ bytes ] = '\0'; /* terminate for printing */
-            printf("%s", fifo_buf);
 
+            memcpy(result_buf, fifo_buf, strlen(fifo_buf));
+            if(input_fptr == stdin)
+                printf("%s", fifo_buf);
+            started_flag = 1; /* for next iterations */
+
+retry:
             /* reset fifo buf for resending */
             memcpy(fifo_buf, client_fifo_name, client_fifo_name_len);
             fifo_buf[client_fifo_name_len] = ' '; /*separate fifo name with a space*/
 
             /* reset place for numbers in the buffer */
             memset(fifo_buf + client_fifo_name_len +1, 0, 4096 -  client_fifo_name_len - 1 );
-        }
-        /* reached when EOF is reached when reading directed input from a file */
-        /* Another "Enter number:" was printed before EOF was detected.
-           To remove it: 
+
+
+            printf("Enter numbers: "); fflush(stdout);
+            fgets_ptr = fgets(fifo_buf + client_fifo_name_len +1,
+                              4096 -  client_fifo_name_len - 1, input_fptr);
+            feof_flag = feof(stdin);
+            /* simutale redirected input behaviour for debugging */
+            #ifdef INPUT_REDIRECT_FLAG
+            fgets_ptr = NULL; feof_flag = 1;
+            #endif /* INPUT_REDIRECT_FLAG */
+        }while( fgets_ptr != NULL && feof_flag == 0);
+        /* EOF reached while using input redirection */
+
+        /* (For previous while() version)
+           Another "Enter number:" was printed before EOF was detected.
+           To remove it and its whole line :
            -    1st \r (carriage return) moves cursor to the beginning of its line
-           -    Spaces overwrite the printed "Enter numbers:"
+           -    Enough spaces to overwrite the printed "Enter numbers:"
            -    2nd \r moves cursor again to the beginning of the line
            -    Then \b (backspace) removes the new line character before the cursor
         */
-        printf("\r              \r\b");
+        /* printf("\r              \r\b"); */
+        /* For current version with do-while() */
+        printf("\b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b \b\b");
+
+        /* print the re-directed input as it is not yet written on the console */
+        printf(" %s\n", tmp_fifo_buf); /* \n flushes stdout */
+        printf("%s", result_buf); /* already contains \n*/
     }
     else{
         /*values passed via argv */
